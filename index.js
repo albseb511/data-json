d8e0b889faa9293e598b3757e898bccbaac235c1
albseb511 <albertsebastian511@gmail.com>
2019-12-07 11:47:13 +0530

    endpoint users added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')
 var cors = require('cors')
 var json = require('./students.json')
+var users = require('./users.json')
 var port = 8000
 app = app()
 app.use(cors())
@@ -10,8 +11,13 @@ app.get('/students',(req,res)=>{
     res.json(json)
 })
 
+app.get('/users',(req,res)=>{
+    res.json(users)
+})
+
 
 app.listen(port,()=>{
     console.log('server runnign at port',port)
     console.log('use http://localhost:8000/students for the api call')
+    console.log('use http://localhost:8000/users for user data')
 })
